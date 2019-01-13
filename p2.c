0cbd98a4da1af87a84b51c260a2b10bf161fb001
Aayushi <aayushisingh3071@gmail.com>
2019-01-13 23:21:43 +0530

    added comments

--- p2.c
+++ p2.c
@@ -4,10 +4,10 @@ int main() {
     long sum = 0, num1 = 1, num2 = 2, numnxt = 0;
     
     while(numnxt <= 4000000)    {
-        numnxt = num1 + num2;
+        numnxt = num1 + num2;    //calculate the next number of the Fibonacci sequence
         
         if(numnxt % 2 == 0)
-             sum+= numnxt;       
+             sum+= numnxt;       //calculate sum of even numbers
                     
         num1 = num2;
         num2 = numnxt; 
